a7a6310f60ad09a91fe4b81532f3fa390e7bd226
shr-samir <linkwithsamir@gmail.com>
2024-10-13 17:41:36 +0545

    1.1 - Create first component

---
+++ 1-components/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Greet from "./components/Greet";
+
+function App() {
+  return (
+    <>
+      <Greet />
+    </>
+  );
+}
+
+// const Greet = () => {
+//   return (
+//     <div>Greetings everyone !!!  Hello from Greet.jsx</div>
+//   )
+// }
+
+export default App;
---
+++ 1-components/src/components/Greet.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Greet = () => {
+  return (
+    <>
+      <h1>Greetings !!! Hello from Greet.jsx</h1>
+      <p>React Component</p>
+    </>
+  );
+};
+
+export default Greet;
