580e6225aaff552dc3e03d47d13c928dc92d3fd7
shr-samir <linkwithsamir@gmail.com>
2024-10-14 05:18:50 +0545

    1.3 - jsx challenge

--- 1-components/src/App.jsx
+++ 1-components/src/App.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
     <div>
       {/* <Greet /> */}
+      <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
